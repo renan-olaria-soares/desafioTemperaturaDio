4f4ffcca309d6bcab75af81ebf1487ab99f7bd24
Renan Silva Soares <102611802+renan-olaria-soares@users.noreply.github.com>
2023-05-16 20:56:18 -0300

    Add files via upload
    Mais um desafio do curso da Digital Innovation One - DIO no curso de Linguagem GO

---
+++ desafioNumerio.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 1; i <= 100; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("PIN PAN")
+		} else if i%5 == 0 {
+			fmt.Println("PAN")
+		} else if i%3 == 0 {
+			fmt.Println("PIN")
+
+		}
+
+	}
+}
